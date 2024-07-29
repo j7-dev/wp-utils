b7df87a73a448ed5bb706d2b1290dbb463532e70
J7 <j7.dev.gg@gmail.com>
2024-07-29 16:52:44 +0800

    feat: 新增 comment case to get_data_fields

--- src/classes/WP.php
+++ src/classes/WP.php
@@ -290,6 +290,23 @@ abstract class WP {
 				'role',
 				'locale',
 			],
+			'comment' => [
+				'comment_post_ID',
+				'comment_author',
+				'comment_author_email',
+				'comment_author_url',
+				'comment_author_IP',
+				'comment_date',
+				'comment_date_gmt',
+				'comment_content',
+				'comment_karma',
+				'comment_approved',
+				'comment_agent',
+				'comment_type',
+				'comment_parent',
+				'comment_meta',
+				'user_id',
+			],
 			default => [],
 		};
 	}
