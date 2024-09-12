1cca2a9085a25eec3f075314513fdac64e72b548
J7 <j7.dev.gg@gmail.com>
2024-09-12 12:04:57 +0800

    feat: 改寫 check_basic_auth

--- src/classes/Auth.php
+++ src/classes/Auth.php
@@ -23,19 +23,40 @@ abstract class Auth {
 	 *
 	 * @example:
 	 * \register_rest_route(
-	 *       'v1/api',
-	 *       'posts',
+	 *       'API_DOMAIN',
+	 *       'YOUR_ENDPOINT',
 	 *       array(
 	 *           'methods'             => 'GET',
-	 *           'callback'            => array( $this, 'get_posts_callback' ),
-	 *           'permission_callback' => array( $this, 'check_basic_auth' ),
+	 *           'callback'            => [ $this, 'YOUR_ENDPOINT_CALLBACK' ],
+	 *           'permission_callback' => [ \J7\WpUtils\Classes\Auth, 'check_basic_auth' ],
 	 *       )
 	 * );
+	 * @see https://developer.wordpress.org/reference/functions/user_can/
+	 * @param string $capability 權限名稱
+	 * @param mixed  ...$args 額外的參數
 	 *
-	 * @param \WP_REST_Request $request - the request object.
 	 * @return \WP_Error|bool
 	 */
-	public static function check_basic_auth( $request ) {
+	public static function check_basic_auth( $capability = 'manage_options', ...$args ): \WP_Error|bool {
+		// 驗證使用者名稱和密碼
+		$user = self::get_user_by_basic_auth();
+		if ( \is_wp_error( $user ) ) {
+			return $user;
+		}
+
+		return \user_can( $user, $capability, ...$args );
+	}
+
+	/**
+	 * 檢查基本驗證 Basic Auth
+	 * 當你需要一個使用 WP 帳號密碼的簡單的驗證機制時，可以使用 Basic Auth。
+	 * 帳號: WP 帳號的 username
+	 * 密碼: 需要到 wp-admin/user-edit.php?user_id=1 這樣的網址中設定`應用程式密碼`
+	 *
+	 * @return \WP_Error|\WP_User
+	 */
+	public static function get_user_by_basic_auth(): \WP_Error|\WP_User {
+
 		// 檢查PHP_AUTH_USER和PHP_AUTH_PW是否設置
 		if ( ! isset( $_SERVER['PHP_AUTH_USER'] ) || ! isset( $_SERVER['PHP_AUTH_PW'] ) ) {
 			return new \WP_Error( 'rest_forbidden', 'Authorization header missing', [ 'status' => 401 ] );
@@ -43,11 +64,8 @@ abstract class Auth {
 
 		// 驗證使用者名稱和密碼
 		$user = \wp_authenticate( $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW'] ); // phpcs:ignore
-		if ( \is_wp_error( $user ) ) {
-			return new \WP_Error( 'rest_forbidden', 'Invalid username or password', [ 'status' => 401 ] );
-		}
 
-		return true;
+		return $user;
 	}
 
 	/**
